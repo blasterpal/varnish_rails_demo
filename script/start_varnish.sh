db4d2bbe741fe295b96080bc913f12d4c41df21c
Hank Beaver <hbeaver@gmail.com>
2009-10-12 08:05:06 -0400

    cleaned up junk

--- script/start_varnish.sh
+++ script/start_varnish.sh
@@ -1,7 +1,3 @@
-if [-z $1] then;
-  echo "You must supply a VCL file path!"
-fi
-
 #Backend host "localhost": resolves to multiple IPv6 addresses.
 #Only one address is allowed.
 #Please specify which exact address you want to use, we found these:
@@ -9,16 +5,14 @@ fi
 #/usr/local/sbin/varnishd -a blackbook.local:3000 -b blackbook.local:8080 -T blackbook.local:6082 
 
 
-#start up Varnish, listening on localhost:8080, with default backend to Rails on 3000, management on port 6082, storage set for 100m in /tmp, 
-/usr/local/sbin/varnishd -a localhost:8080 -b blackbook.local:3000 -T localhost:6082 -F -s file,/tmp/varnish.storage,100M            
-
+#start up Varnish, listening on localhost:8080, with default backend to Rails on 3000, management on port 6082, storage set for 50m in /tmp, 
 
 /usr/local/sbin/varnishd -a localhost:6081 -T localhost:6082 -F -s file,/tmp/varnish.storage,50M -f /Users/hbeaver/code/varnish_demo/config/app.vcl  
-                                       
-
-/usr/local/sbin/varnishd -f  ~/code/varnish_demo/config/app.vcl  
-
-
-
-vcl.load app_conf  /Users/hbeaver/code/varnish_demo/config/app.vcl   
-vcl.use app_conf
+ 
+ 
+                                      
+#Admin examples
+# 
+# 
+# vcl.load app_conf  /Users/hbeaver/code/varnish_demo/config/app.vcl   
+# vcl.use app_conf              
